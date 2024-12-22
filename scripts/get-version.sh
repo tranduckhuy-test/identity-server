d7c8124d0e46593de6bb790a78e704807c2ee7c6
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-12-22 09:58:54 +0700

    add: build image action

---
+++ scripts/get-version.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 [1]"
+    exit 1
+fi
+
+if [ "$1" -eq 1 ]; then
+    CSPROJ_FILE="IdentityServer/IdentityServer.csproj"
+else
+    echo "Invalid option"
+    exit 1
+fi
+
+if [ ! -f "$CSPROJ_FILE" ]; then
+    echo "File not found: $CSPROJ_FILE"
+    exit 1
+fi
+
+VERSION=$(grep -oP '<Version>\K[^<]+' "$CSPROJ_FILE")
+echo "$VERSION"
